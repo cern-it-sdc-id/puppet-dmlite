f4e07f91e3cea672c333a53887c61693c6286db9
Martin Hellmich <mhellmic@gmail.com>
2013-09-23 14:28:05 +0200

    add support for the lcgdm1182 update of $::gridftp
    does not start the dpm-gsiftp service directly, but send
    the name to the gridftp module and let it start the service.

--- code/manifests/gridftp.pp
+++ code/manifests/gridftp.pp
@@ -7,31 +7,32 @@ class dmlite::gridftp (
     $log_level			= "ALL",
     $port			= 2811
 ) {
+    File["/var/log/dpm-gsiftp"] -> Class[Gridftp::Config]
+    Package["dpm-dsi"] -> Class[Gridftp::Config]
 
     package{"dpm-dsi": ensure => present}
 
     # gridftp configuration
-    file { 
-	"/var/log/dpm-gsiftp": 
-		ensure => directory 
-    }
-    class{"gridftp::config": 
-	auth_level		=> 0,
-	detach			=> $detach,
-	disable_usage_stats	=> $disable_usage_stats,
-	load_dsi_module 	=> "dmlite",
-	log_single		=> $log_single,
-	log_transfer		=> $log_transfer,
-	log_level		=> $log_level,
-	login_msg		=> "Disk Pool Manager (dmlite)",
-	port			=> $port,
-	thread_model		=> "pthread",
-
+    file {
+      "/var/log/dpm-gsiftp":
+      ensure => directory
     }
     class{"gridftp::install":}
-    service{"dpm-gsiftp":
-        ensure  => running,
-        require => Package["dpm-dsi"]
+    class{"gridftp::config":
+      auth_level		=> 0,
+      detach			=> $detach,
+      disable_usage_stats	=> $disable_usage_stats,
+      load_dsi_module 	=> "dmlite",
+      log_single		=> $log_single,
+      log_transfer		=> $log_transfer,
+      log_level		=> $log_level,
+      login_msg		=> "Disk Pool Manager (dmlite)",
+      port			=> $port,
+      thread_model		=> "pthread",
+      service => "dpm-gsiftp"
+    }
+    class{"gridftp::service":
+      service => "dpm-gsiftp"
     }
 }
 
