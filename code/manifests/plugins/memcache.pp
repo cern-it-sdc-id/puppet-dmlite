8f8024e8ce7b19925d94acbc20e9775a84fd6d95
Martin Hellmich <mhellmic@gmail.com>
2013-09-25 17:39:17 +0200

    fix the memcache class. there is no service ..

--- code/manifests/plugins/memcache.pp
+++ code/manifests/plugins/memcache.pp
@@ -1,9 +1,8 @@
 class dmlite::plugins::memcache {
 
-  Class[Dmlite::Plugins::Memcache::Install] -> Class[Dmlite::Plugins::Memcache::Config] -> Class[Dmlite::Plugins::Memcache::Service]
+  Class[Dmlite::Plugins::Memcache::Install] -> Class[Dmlite::Plugins::Memcache::Config]
 
   class{"dmlite::plugins::memcache::config":}
   class{"dmlite::plugins::memcache::install":}
-  class{"dmlite::plugins::memcache::service":}
 
 }
