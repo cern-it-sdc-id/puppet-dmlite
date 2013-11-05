b5bd309ae59dc39c2c37040f6777f5d788afb6df
Martin Hellmich <mhellmic@gmail.com>
2013-11-05 17:40:04 +0100

    fix whitespaces

--- code/manifests/plugins/adapter/params.pp
+++ code/manifests/plugins/adapter/params.pp
@@ -1,13 +1,13 @@
 class dmlite::plugins::adapter::params (
 ) inherits dmlite::params {
-    $dpm_host		= undef
-    $ns_host		= undef
-    $connection_timeout	= 2
-    $retry_limit	= 3
-    $retry_interval	= 2
-    $enable_dpm		= false
-    $enable_ns		= false
-    $enable_io		= false
-    $enable_rfio	= true
-    $enable_pooldriver	= true
+    $dpm_host           = undef
+    $ns_host            = undef
+    $connection_timeout = 2
+    $retry_limit        = 3
+    $retry_interval     = 2
+    $enable_dpm         = false
+    $enable_ns          = false
+    $enable_io          = false
+    $enable_rfio        = true
+    $enable_pooldriver  = true
 }
