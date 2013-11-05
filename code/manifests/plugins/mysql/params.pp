e68fd0dbc5d4ff8499df7abaeb07d11f9f6c2c8f
Martin Hellmich <mhellmic@gmail.com>
2013-11-05 17:41:24 +0100

    fix whitespaces

--- code/manifests/plugins/mysql/params.pp
+++ code/manifests/plugins/mysql/params.pp
@@ -1,15 +1,15 @@
 class dmlite::plugins::mysql::params (
 ) inherits dmlite::params {
-    $mysql_host		= "localhost"
-    $mysql_username	= "dpmdbuser"
-    $mysql_password	= "change-this"
-    $mysql_port		= 0
-    $ns_db		= "cns_db"
-    $dpm_db		= "dpm_db"
-    $dbpool_size	= 100
-    $mapfile		= "/etc/lcgdm-mapfile"
-    $host_dn_is_root	= "no"
+    $mysql_host       = "localhost"
+    $mysql_username   = "dpmdbuser"
+    $mysql_password   = "change-this"
+    $mysql_port       = 0
+    $ns_db            = "cns_db"
+    $dpm_db           = "dpm_db"
+    $dbpool_size      = 100
+    $mapfile          = "/etc/lcgdm-mapfile"
+    $host_dn_is_root  = "no"
 
-    $enable_dpm		= true
-    $enable_ns		= false
+    $enable_dpm       = true
+    $enable_ns        = false
 }
