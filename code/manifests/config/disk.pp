1b682e57c23856c12da278af92e29de869a4de36
Martin Hellmich <mhellmic@gmail.com>
2013-11-07 11:38:50 +0100

    LCGDM-1225: create config::head and config::disk

---
+++ code/manifests/config/disk.pp
@@ -0,0 +1,33 @@
+class dmlite::config::disk (
+  $token_password = $dmlite::params::token_password,
+  $token_id       = $dmlite::params::token_id,
+  $token_life     = $dmlite::params::token_life,
+  $enable_config  = $dmlite::params::enable_config,
+  $user           = $dmlite::params::user,
+  $group          = $dmlite::params::group
+) inherits dmlite::params {
+
+  Class[Dmlite::Install] -> Class[Dmlite::Config::Disk]
+
+  # the head config is needed for xrootd and gridftp
+  dmlite::create_config{"head_config":
+    config_file_name => "dmlite",
+    user             => $user,
+    group            => $group,
+    token_password   => $token_password,
+    token_id         => $token_id,
+    token_life       => $token_life,
+    enable_config    => $enable_config
+  }
+
+  dmlite::create_config{"disk_config_http":
+    config_file_name => "dmlite-disk",
+    user             => $user,
+    group            => $group,
+    token_password   => $token_password,
+    token_id         => $token_id,
+    token_life       => $token_life,
+    enable_config    => $enable_config
+  }
+}
+
---
+++ code/manifests/config/head.pp
@@ -0,0 +1,31 @@
+class dmlite::config::head (
+  $token_password = $dmlite::params::token_password,
+  $token_id       = $dmlite::params::token_id,
+  $token_life     = $dmlite::params::token_life,
+  $enable_config  = $dmlite::params::enable_config,
+  $user           = $dmlite::params::user,
+  $group          = $dmlite::params::group
+) inherits dmlite::params {
+
+  Class[Dmlite::Install] -> Class[Dmlite::Config::Head]
+
+  dmlite::create_config{"head_config":
+    config_file_name => "dmlite",   # create /etc/dmlite.conf
+    user             => $user,
+    group            => $group,
+    token_password   => $token_password,
+    token_id         => $token_id,
+    token_life       => $token_life,
+    enable_config    => $enable_config
+  }
+
+  dmlite::create_config{"disk_config_http":
+    config_file_name => "dmlite-disk",   # create /etc/dmlite.conf
+    user             => $user,
+    group            => $group,
+    token_password   => $token_password,
+    token_id         => $token_id,
+    token_life       => $token_life,
+    enable_config    => $enable_config
+  }
+}
