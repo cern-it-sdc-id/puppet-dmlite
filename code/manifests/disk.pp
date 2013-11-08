9333efd01c42cd5b079c824d7c221ef8f78062f4
Martin Hellmich <mhellmic@gmail.com>
2013-11-08 13:38:26 +0100

    LCGDM-1224: add convenience class dmlite::disk
    configures dmlite with the adapter for a disk node

---
+++ code/manifests/disk.pp
@@ -0,0 +1,17 @@
+class dmlite::head (
+  $token_password,
+  $dpmhost   = "${::fqdn}",
+  $nshost    = "${::fqdn}",
+  $debuginfo  = false,
+) {
+  class{"dmlite::config::disk":}
+  class{"dmlite::install":
+    debuginfo => "${debuginfo}"
+  }
+  class{"dmlite::plugins::adapter::config::disk":
+    token_password => "${token_password}",
+    dpmhost        => "${dpmhost}",
+    nshost         => "${nshost}"
+  }
+  class{"dmlite::plugins::adapter::install":}
+}
