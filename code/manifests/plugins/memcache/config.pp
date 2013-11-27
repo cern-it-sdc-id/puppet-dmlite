b617dc54b25751acbebce78ab73f35a817202253
Martin Hellmich <mhellmic@gmail.com>
2013-11-27 09:33:46 +0100

    update memcache module with more options

--- code/manifests/plugins/memcache/config.pp
+++ code/manifests/plugins/memcache/config.pp
@@ -1,21 +1,32 @@
 class dmlite::plugins::memcache::config (
-    $enable_memcache  = $dmlite::plugins::memcache::params::enable_memcache,
-    $pool_size        = $dmlite::plugins::memcache::params::pool_size,
-    $user             = $dmlite::params::user,
-    $group            = $dmlite::params::group
+  $servers          = $dmlite::params::servers,
+  $enable_memcache  = $dmlite::plugins::memcache::params::enable_memcache,
+  $pool_size        = $dmlite::plugins::memcache::params::pool_size,
+  $user             = $dmlite::params::user,
+  $group            = $dmlite::params::group,
+  $protocol         = $dmlite::params::protocol,
+  $expiration_limit = $dmlite::params::expiration_limit
 ) inherits dmlite::plugins::memcache::params {
 
-    file {
-      "/etc/dmlite.conf.d/memcache.conf":
-        ensure  => absent;
-    }
+  validate_array($servers)
+  validate_bool($enable_memcache)
+  if is_numeric($expiration_limit) == false {
+    fail("The ${expiration_limit} provided is not a number. Please provide a positive integer.")
+  }
+  validate_re($protocol, '^(ascii|binary)$',
+    "${protocol} is not a valid memcached protocol. It should be 'ascii' or 'binary'.")
 
-    file {
-      "/etc/dmlite.conf.d/zmemcache.conf":
-        owner     => $user,
-        group     => $group,
-        mode    => 0600,
-        content => template("dmlite/plugins/memcache.conf.erb"),
-        require => Package["dmlite-plugins-memcache"]
-    }
+  file {
+    "/etc/dmlite.conf.d/memcache.conf":
+      ensure  => absent;
+  }
+
+  file {
+    "/etc/dmlite.conf.d/zmemcache.conf":
+      owner     => $user,
+      group     => $group,
+      mode    => 0600,
+      content => template("dmlite/plugins/memcache.conf.erb"),
+      require => Package["dmlite-plugins-memcache"]
+  }
 }
--- code/manifests/plugins/memcache/params.pp
+++ code/manifests/plugins/memcache/params.pp
@@ -1,12 +1,12 @@
 class dmlite::plugins::memcache::params (
 ) inherits dmlite::params {
-    $server		= "localhost:11211"
-    $pool_size		= 32
-    $symlink_limit	= 5
-    $expiration_limit	= 60
-    $protocol		= "binary"
-    $hash_distribution	= "default"
-    $strict_consistency	= "off"
+    $servers            = ["localhost:11211"]
+    $pool_size          = 32
+    $symlink_limit      = 5
+    $expiration_limit   = 60
+    $protocol           = "binary"
+    $hash_distribution  = "default"
+    $strict_consistency = "off"
 
-    $enable_memcache	= true
+    $enable_memcache    = true
 }
