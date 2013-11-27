81863228a2a2452fb698b8dcf960fff0bda473d3
Martin Hellmich <mhellmic@gmail.com>
2013-11-27 09:33:46 +0100

    update memcache module with more options

--- code/manifests/plugins/memcache.pp
+++ code/manifests/plugins/memcache.pp
@@ -1,11 +1,23 @@
 class dmlite::plugins::memcache(
-  $pool_size = 32
+  $servers          = $dmlite::params::servers,
+  $enable_memcache  = $dmlite::plugins::memcache::params::enable_memcache,
+  $pool_size        = $dmlite::plugins::memcache::params::pool_size,
+  $user             = $dmlite::params::user,
+  $group            = $dmlite::params::group,
+  $protocol         = $dmlite::params::protocol,
+  $expiration_limit = $dmlite::params::expiration_limit
 )  {
 
   Class[Dmlite::Plugins::Memcache::Install] -> Class[Dmlite::Plugins::Memcache::Config]
 
   class{"dmlite::plugins::memcache::config":
-    pool_size => $pool_size
+    servers          => $servers,
+    enable_memcache  => $enable_memcache,
+    pool_size        => $pool_size,
+    user             => "${user}",
+    group            => "${group}",
+    protocol         => "${protocol}",
+    expiration_limit => $expiration_limit
   }
   class{"dmlite::plugins::memcache::install":}
 
