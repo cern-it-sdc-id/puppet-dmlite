9d7597178fd70f8f4af4fd191d6c7aa443b5d7e8
Martin Hellmich <mhellmic@gmail.com>
2014-07-16 16:11:32 +0200

    enable configuring the main dmlite parameters with hiera

--- code/manifests/params.pp
+++ code/manifests/params.pp
@@ -4,10 +4,10 @@ class dmlite::params {
     default  => "lib",
   }
 
-  $enable_config  = true
-  $user           = $lcgdm::base::config::user
-  $group          = $lcgdm::base::config::user
-  $log_level      = 1
-  $logcomponents  = undef
+  $enable_config  = hiera('dmlite::params::enable_config', true)
+  $user           = hiera('dmlite::params::user', $lcgdm::base::config::user)
+  $group          = hiera('dmlite::params::group', $lcgdm::base::config::user)
+  $log_level      = hiera('dmlite::params::log_level', 1)
+  $logcomponents  = hiera('dmlite::params::logcomponents')
 
 }
