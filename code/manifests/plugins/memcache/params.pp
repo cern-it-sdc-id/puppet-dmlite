500db886f8a42b7bcfce699fad685d2a24952e42
Martin Hellmich <mhellmic@gmail.com>
2014-07-25 11:28:49 +0200

    let dmlite memcache params be configured by hiera

--- code/manifests/plugins/memcache/params.pp
+++ code/manifests/plugins/memcache/params.pp
@@ -1,15 +1,15 @@
 class dmlite::plugins::memcache::params (
 ) inherits dmlite::params {
-    $servers            = ["localhost:11211"]
-    $pool_size          = 250
-    $symlink_limit      = 5
-    $expiration_limit   = 600
-    $protocol           = "binary"
-    $hash_distribution  = "default"
-    $posix              = "on"
-    $func_counter       = "off"
+    $servers               = hiera('dmlite::plugins::memcache::params::servers', ["localhost:11211"])
+    $pool_size             = hiera('dmlite::plugins::memcache::params::pool_size', 250)
+    $symlink_limit         = hiera('dmlite::plugins::memcache::params::symlink_limit', 5)
+    $expiration_limit      = hiera('dmlite::plugins::memcache::params::expiration_limit', 600)
+    $protocol              = hiera('dmlite::plugins::memcache::params::protocol', "binary")
+    $hash_distribution     = hiera('dmlite::plugins::memcache::params::hash_distribution', "default")
+    $posix                 = hiera('dmlite::plugins::memcache::params::posix', "on")
+    $func_counter          = hiera('dmlite::plugins::memcache::params::func_counter', "off")
 
-    $enable_memcache       = true
-    $enable_memcache_cat   = true
-    $enable_memcache_pool  = true
+    $enable_memcache       = hiera('dmlite::plugins::memcache::params::enable_memcache', true)
+    $enable_memcache_cat   = hiera('dmlite::plugins::memcache::params::enable_memcache_cat', false)
+    $enable_memcache_pool  = hiera('dmlite::plugins::memcache::params::enable_memcache_pool', false)
 }
