17df305aa24aafe6b21f8d6f512a814f33654383
Andrea Manzi <andrea.manzi@cern.ch>
2014-07-29 12:09:42 +0200

    adding deps to NS conf, to fix a startup issue

--- code/manifests/srm/service.pp
+++ code/manifests/srm/service.pp
@@ -1,11 +1,14 @@
 class dmlite::srm::service (
 ) inherits dmlite::srm::params {
 
+    Class[Dmlite::Srm::Service] -> Class[Lcgdm::Ns::Install]
+
     service {"srmv2.2":
     	ensure		=> running,
 	hasstatus	=> true,
 	hasrestart	=> true,
 	enable		=> true,
-	require		=> [ Class["dmlite::srm::config"], Class["dmlite::srm::install"] ]
+	require		=> [ Class["dmlite::srm::config"], Class["dmlite::srm::install"] ],
+        subscribe       => File["$lcgdm::ns::config::configfile"]
     }
 }
