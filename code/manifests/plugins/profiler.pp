49eb2a7000aef8b4942d3aa870ca160be3e10a2a
Martin Hellmich <mhellmic@gmail.com>
2014-09-05 12:01:39 +0200

    profiler: remove the service class references

--- code/manifests/plugins/profiler.pp
+++ code/manifests/plugins/profiler.pp
@@ -1,15 +1,14 @@
 class dmlite::plugins::profiler(
   $collectors = $dmlite::plugins::profiler::params::collectors,
-  $auth = $dmlite::plugins::profiler::params::auth,
+  $auth       = $dmlite::plugins::profiler::params::auth,
 ) inherits dmlite::plugins::profiler::params {
 
-  Class[Dmlite::Plugins::Profiler::Install] -> Class[Dmlite::Plugins::Profiler::Config] -> Class[Dmlite::Plugins::Profiler::Service]
+  Class[Dmlite::Plugins::Profiler::Install] -> Class[Dmlite::Plugins::Profiler::Config]
 
   class{"dmlite::plugins::profiler::config":
     collectors => $collectors,
     auth       => $auth
   }
   class{"dmlite::plugins::profiler::install":}
-  class{"dmlite::plugins::profiler::service":}
 
 }
