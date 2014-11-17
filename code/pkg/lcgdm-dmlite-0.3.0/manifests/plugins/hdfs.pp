ebd63f42f433e1dbdae416d0595a68407f82c28b
andrea-manzi <andrea.manzi@gmail.com>
2014-11-17 11:37:34 +0100

    remove pkg folder

--- code/pkg/lcgdm-dmlite-0.3.0/manifests/plugins/hdfs.pp
+++
@@ -1,9 +0,0 @@
-class dmlite::plugins::hdfs {
-
-  Class[Dmlite::Plugins::Hdfs::Install] -> Class[Dmlite::Plugins::Hdfs::Config] -> Class[Dmlite::Plugins::Hdfs::Service]
-
-  class{"dmlite::plugins::hdfs::config":}
-  class{"dmlite::plugins::hdfs::install":}
-  class{"dmlite::plugins::hdfs::service":}
-
-}
--- code/pkg/lcgdm-dmlite-0.3.0/manifests/python.pp
+++
@@ -1,9 +0,0 @@
-class dmlite::python (
-) inherits dmlite::python::params {
-
-  Class[Dmlite::Python::Install] -> Class[Dmlite::Python::Config]
-
-  class{"dmlite::python::install":}
-  class{"dmlite::python::config":}
-
-}
