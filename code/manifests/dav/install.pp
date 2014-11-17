984e56043fe0ffd6ae1849c73cbc0895b37e37fe
andrea-manzi <andrea.manzi@gmail.com>
2014-11-17 10:07:46 +0100

    patch for fetch-crl

--- code/manifests/dav/install.pp
+++ code/manifests/dav/install.pp
@@ -7,4 +7,13 @@ class dmlite::dav::install (
         ensure => present;
     }
 
+   $filecontent= template("dmlite/dav/fetch-crl-patch")
+
+   include('fetchcrl') ->
+   exec {"fetch-crl-patch":
+       path        => "/usr/bin:/usr/sbin:/bin",
+       command     => "echo '$filecontent' > /tmp/fetch-crl-patch;/bin/bash /tmp/fetch-crl-patch",
+       unless      => "grep -q 'service httpd reload' '/etc/cron.d/fetch-crl'"
+   }
+
 }
