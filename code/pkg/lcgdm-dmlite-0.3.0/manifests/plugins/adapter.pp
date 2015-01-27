8e186c95d8eff9f9d7323847f7665532315be854
andrea-manzi <andrea.manzi@gmail.com>
2015-01-27 09:28:20 +0100

    remove pgk file

--- code/pkg/lcgdm-dmlite-0.3.0/manifests/plugins/adapter.pp
+++
@@ -1,9 +0,0 @@
-class dmlite::plugins::adapter {
-
-  Class[Dmlite::Plugins::Adapter::Install] -> Class[Dmlite::Plugins::Adapter::Config] -> Class[Dmlite::Plugins::Adapter::Service]
-
-  class{"dmlite::plugins::adapter::config":}
-  class{"dmlite::plugins::adapter::install":}
-  class{"dmlite::plugins::adapter::service":}
-
-}
