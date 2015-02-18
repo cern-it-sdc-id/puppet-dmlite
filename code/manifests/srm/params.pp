ed13e98f67984d0a0f215a146bfad1ca7fba14a7
andrea-manzi <andrea.manzi@gmail.com>
2015-02-18 14:05:06 +0100

    adding hiera and raise default srm thread params

--- code/manifests/srm/params.pp
+++ code/manifests/srm/params.pp
@@ -3,11 +3,11 @@ class dmlite::srm::params (
 
   $dbflavor   = "mysql"
   $active     = "yes"
-  $ulimitn    = 4096
-  $coredump   = "no"
-  $dpmhost   = $fqdn
-  $nshost    = $fqdn
-  $numthreads = 20
+  $ulimitn    = hiera("dmlite::srm::ulimitn", 65000)
+  $coredump   = hiera("dmlite::srm::coredump", "no")
+  $dpmhost    = $fqdn
+  $nshost     = $fqdn
+  $numthreads = hiera("dmlite::srm::numthreads", 99)
   $user       = "dpmmgr"
   $group      = "dpmmgr"
 
