cdf4b3dd37acbeda5b1e21f6b5531f8448292ca4
andrea-manzi <andrea.manzi@gmail.com>
2015-03-19 23:03:44 +0100

    modified conf for hdfs plugin

--- manifests/head_hdfs.pp
+++ manifests/head_hdfs.pp
@@ -1,6 +1,7 @@
-class dmlite::head (
+class dmlite::head_hdfs (
   $token_password,
   $token_id   = 'ip',
+  $token_life = 1000,
   $mysql_username,
   $mysql_password,
   $mysql_host = 'localhost',
@@ -10,6 +11,13 @@ class dmlite::head (
   $debuginfo  = false,
   $log_level      = 1,
   $logcomponents  = undef,
+  $hdfs_namenode  = undef,
+  $hdfs_port      = undef,
+  $hdfs_user      = undef,
+  $hdfs_mode      = "rw",
+  $hdfs_gateway   = "${::fqdn}",
+  $enable_io      = false,
+  $enable_ns      = false,
 ) {
   class{'dmlite::config::head':
     log_level     => $log_level,
@@ -18,16 +26,22 @@ class dmlite::head (
   class{'dmlite::install':
     debuginfo => $debuginfo
   }
+ 
+  Class[Dmlite::Plugins::Hdfs::Install] -> Class[Dmlite::Plugins::Hdfs::Config] 
 
-  class{'dmlite::plugins::adapter::config::head':
+  class{'dmlite::plugins::hdfs::config':
     token_password => "${token_password}",
     token_id       => "${token_id}",
-    dpmhost        => "${dpmhost}",
-    nshost         => "${nshost}",
-    adminuser      => "${adminuser}",
-    with_db_plugin => true,
+    token_life     => "${token_life}",
+    hdfs_namenode  => "${hdfs_namenode}",
+    hdfs_port      => "${hdfs_port}",
+    hdfs_user      => "${hdfs_user}",
+    hdfs_mode      => "${hdfs_mode}",
+    hdfs_gateway   => "${hdfs_gateway}",
+    enable_io      => "${enable_io}",
+    enable_ns      => "${enable_ns}",
   }
-  class{'dmlite::plugins::adapter::install':}
+  class{'dmlite::plugins::hdfs::install':}
 
   class{'dmlite::plugins::mysql::config':
     mysql_host     => "${mysql_host}",
--- manifests/plugins/hdfs/config.pp
+++ manifests/plugins/hdfs/config.pp
@@ -7,14 +7,16 @@ class dmlite::plugins::hdfs::config (
   $hdfs_port          = $dmlite::plugins::hdfs::params::hdfs_port,
   $hdfs_user          = $dmlite::plugins::hdfs::params::hdfs_user,
   $hdfs_mode          = $dmlite::plugins::hdfs::params::hdfs_mode,
+  $hdfs_gateway       = $dmlite::plugins::hdfs::params::hdfs_gateway,
   $hadoop_home_lib    = $dmlite::plugins::hdfs::params::hadoop_home_lib,
   $hdfs_home_lib      = $dmlite::plugins::hdfs::params::hdfs_home_lib,
   $java_home          = $dmlite::plugins::hdfs::params::java_home,
   $token_password     = $dmlite::plugins::hdfs::params::token_password,
   $token_id           = $dmlite::plugins::hdfs::params::token_id,
   $token_life         = $dmlite::plugins::hdfs::params::token_life,
+  $map_file           = $dmlite::plugins::hdfs::params::map_file,
   $user               = $dmlite::params::user,
-  $group              = $dmlite::params::group
+  $group              = $dmlite::params::group,
 
 ) inherits dmlite::plugins::hdfs::params {
 
