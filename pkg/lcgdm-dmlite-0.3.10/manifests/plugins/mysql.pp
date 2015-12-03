00b307fe46ebe136ee5d7918f6f4f088d5de5240
andrea-manzi <andrea.manzi@gmail.com>
2015-12-03 09:23:21 +0100

    updated changelog

--- pkg/lcgdm-dmlite-0.3.10/manifests/plugins/mysql.pp
+++
@@ -1,9 +0,0 @@
-class dmlite::plugins::mysql {
-
-  Class[Dmlite::Plugins::Mysql::Install] -> Class[Dmlite::Plugins::Mysql::Config] -> Class[Dmlite::Plugins::Mysql::Service]
-
-  class{'dmlite::plugins::mysql::config':}
-  class{'dmlite::plugins::mysql::install':}
-  class{'dmlite::plugins::mysql::service':}
-
-}
