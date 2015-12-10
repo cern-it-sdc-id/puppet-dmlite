acb9ee2d924d869fe727fa44c56ea557e2448528
andrea-manzi <andrea.manzi@gmail.com>
2015-12-10 11:39:04 +0100

    added oss_stat_lib

--- manifests/xrootd/create_redir_config.pp
+++ manifests/xrootd/create_redir_config.pp
@@ -46,6 +46,7 @@ define dmlite::xrootd::create_redir_config (
 
   $pss_setopt = $xrootd::config::pss_setopt,
   $cms_cidtag =  $xrootd::config::cms_cidtag,
+  $oss_statlib = $xrootd::config::oss_statlib,
 
   $dpm_listvoms = undef,
   $dpm_mmreqhost = undef,
