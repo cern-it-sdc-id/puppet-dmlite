cd0dae9d482131112b01b8be454e6b45fde7b91b
andrea-manzi <andrea.manzi@gmail.com>
2015-12-15 10:37:10 +0100

    updated facter

--- lib/facter/dpm-xrootd.rb
+++ lib/facter/dpm-xrootd.rb
@@ -1,7 +1,12 @@
-require 'puppet'
-pkg = Puppet::Type.type(:package).create(:name => "dpm-xrootd")
-Facter.add("dpm_xrootd_version") do
-  setcode do
-    pkg.retrieve[pkg.property(:ensure)]
+#!/usr/bin/ruby
+require 'facter'
+
+version = Facter::Util::Resolution.exec("rpm -qa --queryformat '[%{NAME} %{VERSION}-%{RELEASE}\n]' | egrep 'dpm-xrootd'")
+
+version.each_line do |package|
+  Facter.add("package_#{package.split[0]}".gsub('-','_')) do
+    setcode do
+      "#{package.split[1]}"
+    end
   end
 end
