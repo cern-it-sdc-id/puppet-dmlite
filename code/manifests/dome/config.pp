71c1bc8563b2bd4c2bb6bdbbfe989ffc4660c622
andrea-manzi <andrea.manzi@gmail.com>
2016-02-12 18:16:30 +0100

    added zdome template and conf

--- code/manifests/dome/config.pp
+++ code/manifests/dome/config.pp
@@ -1,12 +1,23 @@
 class dmlite::dome::config (
-  $dmlite_conf        = $dmlite::dav::params::dmlite_conf,
-  $dome_http_port      = 80,
-  $dome_https_port     = 443,
+  $fcgi_dome_path             = $dmlite::dome::params::fcgi_dome_path
+  $fcgi_dome_port             = $dmlite::dome::params::fcgi_dome_port
+  $fcgi_listen_queue_depth    = $dmlite::dome::params::fcgi_listen_queue_depth
+  $fcgi_processes             = $dmlite::dome::params::fcgi_processes
+  $fcgi_idle_timeout          = $dmlite::dome::params::fcgi_idle_timeout
+  $fcgi_dome_initial_env      = $dmlite::dome::params::fcgi_dome_initial_env
+  $dome_head                  = $dmlite::dome::params::dome_head
+  $dome_disk                  = $dmlite::dome::params::dome_disk
 ) inherits dmlite::dome::params {
 
-  #validate_bool($enable_http)
+  validate_bool($dome_head)
+  validate_bool($dome_disk)
 
-  $dome_template = 'dmlite/dav/zlcgdm-dav.conf'
+  $dome_template = 'dmlite/dome/zdome.conf'
+  file {
+    '/etc/httpd/conf.d/zdome.conf':
+      ensure  => present,
+      content => template("${dome_template}");
+  }
 
 
   Class[dmlite::dome::install] -> Class[dmlite::dome::config]
@@ -18,86 +29,4 @@ class dmlite::dome::config (
     selboolean{'httpd_execmem': value => on, persistent => true }
   }
 
-  #enable cors
-  $domain_string = regsubst("${::domain}", '\.', '\\.', 'G')
-  file {
-      '/etc/httpd/conf.d/cross-domain.conf':
-        ensure  => present,
-        owner   => 'root',
-        group   => 'root',
-        content => template('dmlite/dav/cross-domain.conf.erb')
-  }
-
-  #tweaks
-  file_line {
-    'no apache mod_dav':
-      ensure => absent,
-      line   => 'LoadModule dav_module modules/mod_dav.so',
-      path   => '/etc/httpd/conf/httpd.conf';
-    'no apache mod_dav_fs':
-      ensure => absent,
-      line   => 'LoadModule dav_fs_module modules/mod_dav_fs.so',
-      path   => '/etc/httpd/conf/httpd.conf';
-    'apache user':
-      ensure => present,
-      match  => '^User .*',
-      line   => "User ${user}",
-      path   => '/etc/httpd/conf/httpd.conf';
-    'apache group':
-      ensure => present,
-      match  => '^Group .*',
-      line   => "Group ${group}",
-      path   => '/etc/httpd/conf/httpd.conf';
-  }
-  if $coredump_dir {
-    file_line {'apache coredump':
-      ensure => present,
-      line   => "CoreDumpDirectory ${coredump_dir}",
-      path   => '/etc/httpd/conf/httpd.conf'
-    }
-  }
-
-  if $enable_keep_alive {
-  file_line { 'apache keepalive':
-      ensure => present,
-      line   => 'KeepAlive On',
-      path   => '/etc/httpd/conf/httpd.conf',
-      match  => '^KeepAlive .*'
-    }
-  }
-
-  file_line{'mpm model':
-    ensure => present,
-    path   => '/etc/sysconfig/httpd',
-    line   => "HTTPD=${mpm_model}",
-    match  => '^#?HTTPD=/.*'
-  }
-
-  if $ulimit {
-    file_line {'apache ulimit':
-      ensure => present,
-      line   => "ulimit ${ulimit}",
-      path   => '/etc/sysconfig/httpd'
-    }
-  }
- #centOS7 changes
- if $::operatingsystemmajrelease and ($::operatingsystemmajrelease + 0) >= 7 { 
-     file {
-      '/etc/httpd/conf.modules.d/00-dav.conf':
-        ensure  => absent,
-     }
-
-   file_line { 'mpm event':
-      ensure => present,
-      line   => 'LoadModule mpm_event_module modules/mod_mpm_event.so',
-      path   => '/etc/httpd/conf.modules.d/00-mpm.conf',
-      match  => '^#LoadModule mpm_event_module modules/mod_mpm_event.so'
-    }
-
-   file_line { 'mpm prefork':
-      ensure => absent,
-      line   => 'LoadModule mpm_prefork_module modules/mod_mpm_prefork.so',
-      path   => '/etc/httpd/conf.modules.d/00-mpm.conf',
-    }
-  }
 }
--- code/manifests/dome/params.pp
+++ code/manifests/dome/params.pp
@@ -1,10 +1,11 @@
 class dmlite::dome::params (
 ) inherits dmlite::params {
-    $enable_ns          = true
-    $enable_disk        = true
-    $enable_https       = true
-    $enable_http        = true
-    $enable_keep_alive  = true
-    $mpm_model          = '/usr/sbin/httpd.event'
-    $enable_hdfs        = false
+    $fcgi_dome_path     	= '/var/www/fcgi-bin/dome'
+    $fcgi_dome_port     	= 9001
+    $fgci_listen_queue_depth 	= 500
+    $fcgi_processes        	= 1
+    $fcgi_idle_timeout  	= 300
+    $fcgi_dome_initial_env      = 'DOME_CFGFILE=/etc/dome.conf'
+    $dome_head       		= false
+    $dome_disk   	        = true
 }
