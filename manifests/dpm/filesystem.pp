d30b3f67543e03648beda3dae573180d663b9230
andrea-manzi <andrea.manzi@gmail.com>
2016-12-02 16:43:37 +0100

    adapt to new syntax

--- manifests/dpm/filesystem.pp
+++ manifests/dpm/filesystem.pp
@@ -1,6 +1,5 @@
 define dmlite::dpm::filesystem(
   $ensure = present,
-  $headnode = $dmlite::dpm::config::headnode,
   $server = $fqdn,
   $pool = '',
   $fs = '',
@@ -8,23 +7,20 @@ define dmlite::dpm::filesystem(
 
     Class[dmlite::shell::install] -> Dmlite::Dpm::Filesystem <| |>
 
-    ensure_packages(['dpm-python'])
-
     if $ensure == 'present' {
       $cmd = 'fsadd'
       $grep = 'grep -q'
-      $options = "${fs} ${pool} ${server}"
+      $options = "${pool}"
     } else {
       $cmd = 'fsdel'
       $grep = '! grep -q'
-      $options = "${fs} ${server}"
+      $options = ''
     }
 
     exec{"${pool}/${server}:${fs}":
       path    => '/bin:/sbin:/usr/bin:/usr/sbin',
-      command => "dmlite-shell -e \"${cmd} ${options}\"",
+      command => "dmlite-shell -e \"${cmd} ${fs} ${pool_option} ${server}\"",
       unless  => "dmlite-shell -e \"qryconf\" | ( ${grep} \"${server} ${fs}\" )", # subshell required to invert return status with !
-      require => Ensure_packages['dpm-python']
     }
 
 }
--- manifests/dpm/pool.pp
+++ manifests/dpm/pool.pp
@@ -1,14 +1,15 @@
 define dmlite::dpm::pool(
   $pool_type    = 'filesystem',
+  $stype        = 'P',
   $def_filesize = '100M'
 ) {
 
   Class[dmlite::shell::install] -> Dmlite::Dpm::Pool <| |>
 
-  exec{'lcgdm_dpm_pool-${name}':
+  exec{"lcgdm_dpm_pool-${name}":
     path    => '/bin:/sbin:/usr/bin:/usr/sbin',
-    command => "dmlite-shell -e \"pooladd ${name} ${pool_type}\"",
-    unless  => "dmlite-shell -e \"poolinfo\" | grep \"^${name}\""
+    command => "dmlite-shell -e \"pooladd ${name} ${pool_type} ${stype}\"",
+    unless  => "dmlite-shell -e \"qryconf | grep 'POOL ${name}'\"",
   }
 
 }
