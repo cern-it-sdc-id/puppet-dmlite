1fc7ab2385fcf2d6fd0287dbaa3ede3be072621e
andrea-manzi <andrea.manzi@gmail.com>
2016-12-02 18:10:00 +0100

    removed nfs

--- manifests/nfs.pp
+++
@@ -1,9 +0,0 @@
-class dmlite::nfs {
-
-  Class[dmlite::nfs::install] -> Class[dmlite::nfs::config] -> Class[dmlite::nfs::service]
-
-  class{'dmlite::nfs::install':}
-  class{'dmlite::nfs::config':}
-  class{'dmlite::nfs::service':}
-
-}
