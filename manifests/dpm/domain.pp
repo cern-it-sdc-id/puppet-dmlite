e11f46add9e4838edd8bea193581c4ec13b7fd41
andrea-manzi <andrea.manzi@gmail.com>
2017-01-09 14:29:53 +0100

    added acl coonf

--- manifests/dpm/domain.pp
+++ manifests/dpm/domain.pp
@@ -8,11 +8,23 @@ define dmlite::dpm::domain(
   $domainpath = "/${dmlite::dpm::config::basepath}/${name}"
 
   exec { "ns_domain_${name}":
-    path    => '/usr/bin:/usr/sbin',
+    path    => '/bin:/usr/bin:/usr/sbin',
     command => "dmlite-shell -e \"mkdir ${domainpath} p\"",
     unless  => "dmlite-shell -e \"ls ${domainpath}\"",
     require => Class['dmlite::db::ns']
-  }  
+  } ->
+  exec { "ns_basepath_setacl-${name}":
+    path    => '/bin:/usr/bin:/usr/sbin',
+    command => "dmlite-shell -e \"acl /${dmlite::dpm::config::basepath} d:u::rwx,d:g::rwx,d:o::r-x,u::rwx,g::r-x,o::r-x set\"",
+    unless  => "dmlite-shell -e \"acl /${dmlite::dpm::config::basepath} \" | grep default:user: ",
+    require => Class['dmlite::db::ns']
+  } ->
+   exec { "ns_basepath_setacl-domain-${name}":
+    path    => '/bin:/usr/bin:/usr/sbin',
+    command => "dmlite-shell -e \"acl /${domainpath} d:u::rwx,d:g::rwx,d:o::r-x,u::rwx,g::r-x,o::r-x set\"",
+    unless  => "dmlite-shell -e \"acl /${domainpath} \" | grep default:user:",
+    require => Class['dmlite::db::ns']
+  } 
 
 }
 
--- manifests/dpm/vo.pp
+++ manifests/dpm/vo.pp
@@ -9,10 +9,21 @@ define dmlite::dpm::vo(
   $vopath = "/${dmlite::dpm::config::basepath}/${domain}/home/${name}"
   
   exec { "ns_vo_${domain}_${name}":
-    path    => '/usr/bin:/usr/sbin',
+    path    => '/bin:/usr/bin:/usr/sbin',
     command => "dmlite-shell -e \"mkdir ${vopath} p\"; dmlite-shell -e \"groupadd ${name}\"; dmlite-shell -e \"chgrp  ${vopath} ${name}\"; dmlite-shell -e \"chown ${vopath} root\"; dmlite-shell -e \"chmod ${vopath} 775\"",
     unless  => "dmlite-shell -e \"ls ${vopath}\"",
     require => Class['dmlite::db::ns']
+  }
+  ->
+  exec { "ns_basepath_setacl-dpnsbasedir-${domain}-${name}":
+    path        => '/bin:/usr/bin:/usr/sbin:/bin',
+    command     => "dmlite-shell -e \"acl /${dmlite::dpm::config::basepath}/${domain}/home  d:u::rwx,d:g::rwx,d:o::r-x,u::rwx,g::rwx,o::r-x set\"",
+    unless      => "dmlite-shell -e \"acl /${dmlite::dpm::config::basepath}/${domain}/home \" | grep default:user: ",
+  } ->
+   exec { "ns_vopath_setacl-${domain}-${name}":
+    path        => '/bin:/usr/bin:/usr/sbin:/bin',
+    command     => "dmlite-shell -e \"acl ${vopath} d:u::rwx,d:g::rwx,d:o::r-x,u::rwx,g::rwx,o::r-x set\"",
+    unless      => "dmlite-shell -e \"acl ${vopath} \" | grep default:user: ",
   }
 
 }
