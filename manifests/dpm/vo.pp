5ef692dede56da9b885e948e6e1ee2cc12257de2
andrea-manzi <andrea.manzi@gmail.com>
2017-11-13 17:52:54 +0100

    use && for command chain

--- manifests/dpm/vo.pp
+++ manifests/dpm/vo.pp
@@ -10,7 +10,7 @@ define dmlite::dpm::vo(
   
   exec { "ns_vo_${domain}_${name}":
     path    => '/bin:/usr/bin:/usr/sbin',
-    command => "dmlite-shell -e \"mkdir ${vopath} p\"; dmlite-shell -e \"groupadd ${name}\"; dmlite-shell -e \"chgrp  ${vopath} ${name}\"; dmlite-shell -e \"chown ${vopath} root\"; dmlite-shell -e \"chmod ${vopath} 775\"",
+    command => "dmlite-shell -e \"mkdir ${vopath} p\" && dmlite-shell -e \"groupadd ${name}\" && dmlite-shell -e \"chgrp  ${vopath} ${name}\" && dmlite-shell -e \"chown ${vopath} root\" && dmlite-shell -e \"chmod ${vopath} 775\"",
     unless  => "dmlite-shell -e \"ls ${vopath}\"",
     require => Class['dmlite::db::ns']
   }
