4815f3df21961e7ac0fafb6204c85eb5d73a5208
andrea-manzi <andrea.manzi@gmail.com>
2017-11-20 15:58:56 +0100

    config gai.conf even if is not present

--- manifests/gaiconfig.pp
+++ manifests/gaiconfig.pp
@@ -5,7 +5,6 @@ class dmlite::gaiconfig (
   exec {'set-gai-config':
     path    => '/usr/bin:/usr/sbin:/bin',
     command => "echo '${filecontent}' > /etc/gai.conf",
-    onlyif  => 'ls /etc/gai.conf',
     unless  => 'test -s /etc/gai.conf'
   }
 }
