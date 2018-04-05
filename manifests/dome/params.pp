9f0862e0415def21d7e2c9b394853ea51ac6491d
andrea-manzi <andrea.manzi@gmail.com>
2018-04-05 14:12:46 +0200

    change defaults for checksums

--- manifests/dome/params.pp
+++ manifests/dome/params.pp
@@ -6,8 +6,8 @@ class dmlite::dome::params (
     $disk_debug 	= hiera('dmlite::dome::params::disk_debug', 1)
     $head_maxfilepulls   = hiera('dmlite::dome::params::head_maxfilepulls', 4)
     $head_maxfilepullspernode =  hiera('dmlite::dome::params::head_maxfilepullspernode', 2)
-    $head_maxchecksums  = hiera('dmlite::dome::params::head_maxchecksums', 2)
-    $head_maxchecksumspernode  = hiera('dmlite::dome::params::head_maxchecksumspernode', 2)
+    $head_maxchecksums  = hiera('dmlite::dome::params::head_maxchecksums', 200)
+    $head_maxchecksumspernode  = hiera('dmlite::dome::params::head_maxchecksumspernode', 100)
     $db_host 		= hiera('dmlite::dome::params::db_host', 'localhost')
     $db_user  		= hiera('dmlite::dome::params::db_user', 'dpmmgr')
     $db_password	= undef
