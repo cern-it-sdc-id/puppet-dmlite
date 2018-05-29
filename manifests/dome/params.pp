a91a5442f133c0f5661f9341ff259fce6b2a964c
andrea-manzi <andrea.manzi@gmail.com>
2018-05-29 13:42:49 +0200

    add conf of the db names

--- manifests/dome/params.pp
+++ manifests/dome/params.pp
@@ -13,6 +13,8 @@ class dmlite::dome::params (
     $db_password	= undef
     $db_port		= hiera('dmlite::dome::params::db_port', 0)
     $db_pool_size	= hiera('dmlite::dome::params::db_pool_size', 128)
+    $cnsdb_name         = hiera('dmlite::dome::params::cnsdb_name', 'cns_db')
+    $dpmdb_name         = hiera('dmlite::dome::params::dpmdb_name', 'dpm_db')
     $head_task_maxrunningtime = hiera('dmlite::dome::params::head_task_maxrunningtime', 3600)
     $head_task_purgetime = hiera('dmlite::dome::params::head_task_purgetime', 3600)
     $disk_task_maxrunningtime = hiera('dmlite::dome::params::disk_task_maxrunningtime', 3600)
@@ -24,10 +26,10 @@ class dmlite::dome::params (
     $restclient_cli_certificate = hiera('dmlite::dome::params::restclient_cli_certificate','/etc/grid-security/dpmmgr/dpmcert.pem')
     $restclient_cli_private_key = hiera('dmlite::dome::params::restclient_cli_private_key','/etc/grid-security/dpmmgr/dpmkey.pem')
     $head_filepuller_stathook = hiera('dmlite::dome::params::head_filepuller_stathook','/usr/share/dmlite/filepull/externalstat_example.sh')
-    $head_filepuller_stathooktimeout = hiera('dmlite::dome::params::head_filepuller_stathooktimeout',60)
+    $head_filepuller_stathooktimeout = hiera('dmlite::dome::params::head_filepuller_stathooktimeout',0)
     $disk_filepuller_pullhook = hiera('dmlite::dome::params::disk_filepuller_pullhook','/usr/share/dmlite/filepull/externalpull_example.sh')
     $filepuller = undef
-    $headnode_domeurl = hiera('dmlite::dome::params::disk_filepuller_pullhook',undef)
+    $headnode_domeurl = hiera('dmlite::dome::params::headnode_domeurl',undef)
     $proxy_timeout = hiera('dmlite::dome::params::proxy_timeout',600)
     $restclient_cli_xrdhttpkey = hiera('mlite::dome::params::restclient_cli_xrdhttpkey',undef)
 }
