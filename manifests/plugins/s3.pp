73caaeb2371bdfdfb22c46926bc5c38903094c8d
Andrea Manzi <andrea.manzi@gmail.com>
2019-03-26 16:13:07 +0100

    remove conf conf for unsupported plugins

--- manifests/plugins/s3.pp
+++
@@ -1,9 +0,0 @@
-class dmlite::plugins::s3 {
-
-  Class[dmlite::plugins::s3::install] -> Class[dmlite::plugins::s3::config] -> Class[dmlite::plugins::s3::service]
-
-  class{'dmlite::plugins::s3::config':}
-  class{'dmlite::plugins::s3::install':}
-  class{'dmlite::plugins::s3::service':}
-
-}
